6061b54a288dcaa9d0d8cd49651a478de6cc65d8
harsh <harshitchandel318@gmail.com>
2026-05-13 03:54:00 +0530

    Add BaseTest and BasePage to base package

---
+++ src/test/java/com/sdet/base/BasePage.java
@@ -0,0 +1,12 @@
+package com.sdet.base;
+
+import com.microsoft.playwright.Page;
+
+public class BasePage {
+
+    protected Page page;
+
+    public BasePage(Page page) {
+        this.page = page;
+    }
+}
