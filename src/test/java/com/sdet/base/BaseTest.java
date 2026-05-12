6b7b864a8a2d95120fe7cbf15eece12bfc7495b6
harsh <harshitchandel318@gmail.com>
2026-05-13 05:07:11 +0530

    Set headless mode to true for CI compatibility

--- src/test/java/com/sdet/base/BaseTest.java
+++ src/test/java/com/sdet/base/BaseTest.java
@@ -15,7 +15,7 @@ public class BaseTest {
     public void setUp() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
+                new BrowserType.LaunchOptions().setHeadless(true)
         );
         context = browser.newContext();
         page = context.newPage();
